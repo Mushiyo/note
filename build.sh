c32cd0adafa81d3655e20a144b5cd2292c680ed1
Mushiyo <Mushiyo@gmail.com>
2017-05-02 21:11:09 +0800

    change MathJax url to cdnjs since MathJax CDN was shutting down
    MathJax's announcement: https://www.mathjax.org/cdn-shutting-down/

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "build web pages with Pandoc"
 find CS -name \*.md -type f -exec pandoc --template=pandoc/toc-sidebar.html -B pandoc/nav1 --toc --toc-depth=4 -f markdown_github-ascii_identifiers -o public/{}.html {} \;
 find language -name \*.md -type f -exec pandoc --template=pandoc/toc-sidebar.html -B pandoc/nav1 --toc --toc-depth=4 -f markdown_github-ascii_identifiers -o public/{}.html {} \;
 find other -name \*.md -type f -exec pandoc --template=pandoc/toc-sidebar.html -B pandoc/nav1 --toc --toc-depth=4 -f markdown_github-ascii_identifiers -o public/{}.html {} \;
-find math -name \*.md -type f -exec pandoc --template=pandoc/toc-sidebar.html -B pandoc/nav1 --toc --toc-depth=4 -f markdown_github-ascii_identifiers+tex_math_single_backslash --mathjax -o public/{}.html {} \;
+find math -name \*.md -type f -exec pandoc --template=pandoc/toc-sidebar.html -B pandoc/nav1 --toc --toc-depth=4 -f markdown_github-ascii_identifiers+tex_math_single_backslash --mathjax=https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML -o public/{}.html {} \;
 find programming -name \*.md -type f -exec pandoc --template=pandoc/toc-sidebar.html -B pandoc/nav1 --toc --toc-depth=4 -f markdown_github-ascii_identifiers -o public/{}.html {} \;
 find . -depth -name '*.md.html' -execdir bash -c 'mv -i "$1" "${1//md.html/html}"' bash {} \;
 pandoc index.md --template=pandoc/toc-sidebar.html -B pandoc/nav0 -f markdown_github-ascii_identifiers -o public/index.html;
